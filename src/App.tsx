8ed1924cb361b1cd14085edaa0eae91751efe9a5
raulmar <raulhdz40@gmail.com>
2022-09-22 20:43:48 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import FiveSixSevenDance from "./containers/FiveSixSevenDance";
+
+export default function App() {
+  return (
+    <>
+      <FiveSixSevenDance />
+    </>
+  )
+}
---
+++ src/containers/FiveSixSevenDance.tsx
@@ -0,0 +1,13 @@
+import './FiveSixSevenDance.css';
+
+export default function FiveSixSevenDance() {
+  return (
+    <>
+      <div className="main-container">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
