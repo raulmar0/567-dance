c190a773c6302538c45a58dc60ca329882c09ce1
raulmar <raulhdz40@gmail.com>
2022-09-24 19:56:45 -0500

    normally checked inputs and more organized functions

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,14 +5,18 @@ import { useState } from 'react'
 export default function Footer() {
   const [count, setCount] = useState('120')
 
+  function handleChange(event: any) {
+    setCount(event.target.value)
+  }
+
   return (
     <footer>
       <div className="bpm">
-        <input type="number" value={count} onInput={(event) => setCount(event.target.value)}/>
+        <input type="number" value={count} onInput={handleChange}/>
         bpm
       </div>
       <div className="bpm-slider">
-        <input type="range" onChange={(event) => setCount(event.target.value)} value={count} min="40" max="250" step="10"/>
+        <input type="range" onChange={handleChange} value={count} min="40" max="250" step="10"/>
       </div>
       <div className="toggle-reproduction">
         <img src={PlayButton} alt=""/>
