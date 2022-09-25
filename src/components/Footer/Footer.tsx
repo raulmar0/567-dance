ea1e5b85beaec69adab6f40d4408b28b3ee8bf8c
raulmar <raulhdz40@gmail.com>
2022-09-24 19:48:30 -0500

    linkedBpmInputs

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,18 @@
 import './Footer.css'
 import PlayButton from '../../assets/play.png'
+import { useState } from 'react'
 
 export default function Footer() {
+  const [count, setCount] = useState('120')
+
   return (
     <footer>
       <div className="bpm">
-        120 bpm
+        <input type="number" value={count} onInput={(event) => setCount(event.target.value)}/>
+        bpm
       </div>
       <div className="bpm-slider">
-        <input type="range" />
+        <input type="range" onChange={(event) => setCount(event.target.value)} value={count} min="40" max="250" step="10"/>
       </div>
       <div className="toggle-reproduction">
         <img src={PlayButton} alt=""/>
